b52561cf2dc777d2088775a8dc71b90737f05467
Martijn Kamphuis <mkamphuis@outlook.com>
2021-01-14 21:07:34 +0100

    Fix privacy registry

--- workshops/kubernetes/scripts/disable-privacyexperience.ps1
+++ workshops/kubernetes/scripts/disable-privacyexperience.ps1
@@ -1,1 +1,14 @@
-Set-ItemProperty -Path "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\OOBE" -Name DisablePrivacyExperience -Value 1
+$regPath = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\OOBE"
+
+If(Get-ItemProperty -Path $regPath)
+{
+    Write-Host "Enabling DisablePrivacyExperience value..."
+    Set-ItemProperty -Path $regPath -Name DisablePrivacyExperience -Value 1
+}
+Else
+{
+    Write-Host "Creating DisablePrivacyExperience registry key..."
+    New-ItemProperty -Path $regPath -Name DisablePrivacyExperience -PropertyType DWord -Value 1
+}
+
+Write-Host "Policy change completed."
