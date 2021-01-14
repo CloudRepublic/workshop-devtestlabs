568f2076d556f23c7a98eb95ef0a38f9b992c5fe
Martijn Kamphuis <mkamphuis@outlook.com>
2021-01-14 14:01:26 +0100

    Restart Docker after applying K8s setting

--- workshops/kubernetes/scripts/enable-kubernetes-setting.ps1
+++ workshops/kubernetes/scripts/enable-kubernetes-setting.ps1
@@ -4,8 +4,14 @@ param(
 
 $filepath = "C:\Users\$username\AppData\Roaming\Docker\settings.json"
 
+# Fetch settings data
 $data = Get-Content -Path $dockerSettingsFile | ConvertFrom-Json
 
+# Enable Kubernetes
 $data.kubernetesEnabled = $True
 
+# Write back settings
 Set-Content -Value ($data | ConvertTo-Json) -Path $dockerSettingsFile
+
+# Restart Docker service
+Restart-Service -DisplayName "Docker Desktop Service"
