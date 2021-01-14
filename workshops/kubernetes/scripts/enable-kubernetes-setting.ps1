0b2893cb3bb438127f1d0807798bb638ec95412f
Martijn Kamphuis <mkamphuis@outlook.com>
2021-01-14 14:17:35 +0100

    Adds logging to K8s setting change script

--- workshops/kubernetes/scripts/enable-kubernetes-setting.ps1
+++ workshops/kubernetes/scripts/enable-kubernetes-setting.ps1
@@ -1,5 +1,7 @@
 $filepath = Join-Path -Path $env:APPDATA -ChildPath "/docker/settings.json"
 
+Write-Host "Changing settings on file '$filepath'..."
+
 # Fetch settings data
 $data = Get-Content -Path $filepath | ConvertFrom-Json
 
@@ -9,5 +11,9 @@ $data.kubernetesEnabled = $True
 # Write back settings
 Set-Content -Value ($data | ConvertTo-Json) -Path $filepath
 
+Write-Host "Settings changed."
+
 # Restart Docker service
+Write-Host "Restarting Docker..."
 Restart-Service -DisplayName "Docker Desktop Service"
+Write-Host "Docker restarted."
